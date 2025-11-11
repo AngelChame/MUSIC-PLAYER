fec25e6cde1cc14ef0b7f74b52b62cfaf1a43797
AngelChame <chapperzz3@gmail.com>
2025-11-10 21:14:17 -0600

    update app structure and project configuration

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,49 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
+  standalone: false,
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('MUSIC-PLAYER');
+  title = 'Music Player';
+searchQuery: string = '';
+  searchResults: any[] = [];
+  private debounceTimer: any;
+
+  constructor(private router: Router) {}
+
+
+  onSearchDebounced() {
+    clearTimeout(this.debounceTimer);
+
+
+    this.debounceTimer = setTimeout(() => {
+      if (this.searchQuery.trim() !== '') {
+        this.onSearch();
+      } else {
+        this.searchResults = []; // Limpia resultados si el campo está vacío
+      }
+    }, 400);
+  }
+
+  onSearch(): void {
+    if (this.searchQuery.trim()) {
+      this.router.navigate(['/search'], {
+        queryParams: { q: this.searchQuery }
+      });
+    }
+  }
+
+  backToHome(): void {
+    this.router.navigate(['/']);
+    this.searchQuery = '';
+  }
+
+  isSearchRoute(): boolean {
+    return this.router.url.includes('/search');
+  }
 }
